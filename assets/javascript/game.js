5b25d0e913a56f9319a511bbda3b3499453af4ee
TreezCode <TreezCode@users.noreply.github.com>
2019-04-02 20:24:22 -0600

    reset function update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,6 +10,7 @@ var lettersInWord = [];
 var numOfLetters = 0;
 var hiddenWord = [];
 var wrongLetters = [];
+var pause = false;
 
 // Score counters
 var winCount = 0;
@@ -18,7 +19,7 @@ var guessesLeft = 13;
 
 // FUNCTIONS
 // =====================================================
-function startGame() {
+var reset = () => {
 
     selectWord = wordBank[Math.floor(Math.random() * wordBank.length)];
     lettersInWord = selectWord.split("");
@@ -31,6 +32,7 @@ function startGame() {
     hiddenWord = [];
     hangmanIndex = 1;
     hangmanImage.setAttribute("src", "assets/images/hangman0.png");
+    pause = false;
 
     // Creates underscores to hide word in HTML.
     for (var i = 0; i < numOfLetters; i++) {
@@ -49,34 +51,25 @@ function startGame() {
     document.getElementById("winCounter").innerHTML = winCount;
     document.getElementById("lossCounter").innerHTML = lossCount;
 
-    // Testing
-    // console.log(selectWord);
-    // console.log(lettersInWord);
-    // console.log(numOfLetters);
-    // console.log(hiddenWord);
 }
 
-function checkLetters(letter) {
+var checkLetters = (letter) => {
 
     if (event.keyCode >= 65 && event.keyCode <= 90) {
 
-        // var userGuess = event.key
+        var alreadyGuessed = false;
         if (wrongLetters.includes(letter)) {
             document.getElementById("alert-text").textContent = "You've already guessed that letter.";
+            alreadyGuessed = true;
         }
-
-        // Check if letter exists in code at all
+        // Check if letter exists in code
         var isLetterInWord = false;
         for (var i = 0; i < numOfLetters; i++) {
             if (selectWord[i] == letter) {
                 isLetterInWord = true;
             }
-            if (selectWord[i] === "&nbsp;") {
-                isLetterInWord = true;
-            }
         }
-
-        // Check where in the word letter exists, then populate blanksandsuccesses array
+        // Check where the letter exists then populate it in hidden word array
         if (isLetterInWord) {
             for (var i = 0; i < numOfLetters; i++) {
                 if (selectWord[i] == letter) {
@@ -84,18 +77,15 @@ function checkLetters(letter) {
                 }
             }
         }
-
-        // Letter wasn't found
-        else {
+        if (isLetterInWord === false && alreadyGuessed === false && pause === false) {
             wrongLetters.push(letter);
             changeHangman();
             guessesLeft--
         }
-        // console.log(hiddenWord);
     }
 }
 
-function roundComplete() {
+var roundComplete = () => {
 
     // Update the HTML to reflect the most recent count stats
     document.getElementById("numGuesses").innerHTML = guessesLeft;
@@ -103,143 +93,141 @@ function roundComplete() {
     document.getElementById("wrongGuesses").innerHTML = wrongLetters.join(" ");
 
     // Cheeck if user won
-    if (lettersInWord.toString() == hiddenWord.toString()) {
-        // Add wins
+    if (lettersInWord.toString() == hiddenWord.toString() && pause === false) {
         winCount++;
-       
-        // Display image on win
         switch (selectWord) {
             case "all that":
-            hangmanImage.setAttribute("src", "assets/images/allthat.gif");
-            break;
+                hangmanImage.setAttribute("src", "assets/images/allthat.gif");
+                break;
             case "angry beavers":
-            hangmanImage.setAttribute("src", "assets/images/angry-beavers.png");
-            break;
+                hangmanImage.setAttribute("src", "assets/images/angry-beavers.png");
+                break;
             case "arnold":
-            hangmanImage.setAttribute("src", "assets/images/arnold.jpg");
-            break;
+                hangmanImage.setAttribute("src", "assets/images/arnold.jpg");
+                break;
             case "angelica":
-            hangmanImage.setAttribute("src", "assets/images/angelica.jpg");
-            break;
+                hangmanImage.setAttribute("src", "assets/images/angelica.jpg");
+                break;
             case "catdog":
-            hangmanImage.setAttribute("src", "assets/images/catdog.jpg");
-            break;
+                hangmanImage.setAttribute("src", "assets/images/catdog.jpg");
+                break;
             case "chuckie":
-            hangmanImage.setAttribute("src", "assets/images/chuckie.jpg");
-            break;
+                hangmanImage.setAttribute("src", "assets/images/chuckie.jpg");
+                break;
             case "courage":
-            hangmanImage.setAttribute("src", "assets/images/courage-cowardly-dog.jpg");
-            break;
+                hangmanImage.setAttribute("src", "assets/images/courage-cowardly-dog.jpg");
+                break;
             case "cow":
-            hangmanImage.setAttribute("src", "assets/images/cowAndChicken.jpg");
-            break;
+                hangmanImage.setAttribute("src", "assets/images/cowAndChicken.jpg");
+                break;
             case "chicken":
-            hangmanImage.setAttribute("src", "assets/images/cowAndChicken.jpg");
-            break;
+                hangmanImage.setAttribute("src", "assets/images/cowAndChicken.jpg");
+                break;
             case "dexters laboratory":
-            hangmanImage.setAttribute("src", "assets/images/dexters-lab.jpg");
-            break;
+                hangmanImage.setAttribute("src", "assets/images/dexters-lab.jpg");
+                break;
             case "double dare":
-            hangmanImage.setAttribute("src", "assets/images/double_dare_logo.png");
-            break;
+                hangmanImage.setAttribute("src", "assets/images/double_dare_logo.png");
+                break;
             case "doug funny":
-            hangmanImage.setAttribute("src", "assets/images/doug.jpg");
-            break;
+                hangmanImage.setAttribute("src", "assets/images/doug.jpg");
+                break;
             case "eliza":
-            hangmanImage.setAttribute("src", "assets/images/wild_thornberrys.jpg");
-            break;
+                hangmanImage.setAttribute("src", "assets/images/wild_thornberrys.jpg");
+                break;
             case "gerald":
-            hangmanImage.setAttribute("src", "assets/images/gerald.jpg");
-            break;
+                hangmanImage.setAttribute("src", "assets/images/gerald.jpg");
+                break;
             case "global guts":
-            hangmanImage.setAttribute("src", "assets/images/guts.jpg");
-            break;
+                hangmanImage.setAttribute("src", "assets/images/guts.jpg");
+                break;
             case "heffer":
-            hangmanImage.setAttribute("src", "assets/images/heffer.jpg");
-            break;
+                hangmanImage.setAttribute("src", "assets/images/heffer.jpg");
+                break;
             case "helga":
-            hangmanImage.setAttribute("src", "assets/images/helga.jpg");
-            break;
+                hangmanImage.setAttribute("src", "assets/images/helga.jpg");
+                break;
             case "hey arnold":
-            hangmanImage.setAttribute("src", "assets/images/hey arnold.png");
-            break;
+                hangmanImage.setAttribute("src", "assets/images/hey arnold.png");
+                break;
             case "ickis":
-            hangmanImage.setAttribute("src", "assets/images/ahhRealMonsters.jpg");
-            break;
+                hangmanImage.setAttribute("src", "assets/images/ahhRealMonsters.jpg");
+                break;
             case "invader zim":
-            hangmanImage.setAttribute("src", "assets/images/invaderZim.jpg");
-            break;
+                hangmanImage.setAttribute("src", "assets/images/invaderZim.jpg");
+                break;
             case "johnny bravo":
-            hangmanImage.setAttribute("src", "assets/images/johhnyBravo.jpg");
-            break;
+                hangmanImage.setAttribute("src", "assets/images/johhnyBravo.jpg");
+                break;
             case "kablam":
-            hangmanImage.setAttribute("src", "assets/images/kablam.jpg");
-            break;
+                hangmanImage.setAttribute("src", "assets/images/kablam.jpg");
+                break;
             case "krumm":
-            hangmanImage.setAttribute("src", "assets/images/ahhRealMonsters.jpg");
-            break;
+                hangmanImage.setAttribute("src", "assets/images/ahhRealMonsters.jpg");
+                break;
             case "oblina":
-            hangmanImage.setAttribute("src", "assets/images/ahhRealMonsters.jpg");
-            break;
+                hangmanImage.setAttribute("src", "assets/images/ahhRealMonsters.jpg");
+                break;
             case "otto":
-            hangmanImage.setAttribute("src", "assets/images/otto.jpg");
-            break;
+                hangmanImage.setAttribute("src", "assets/images/otto.jpg");
+                break;
             case "patti mayonaise":
-            hangmanImage.setAttribute("src", "assets/images/patti.jpg");
-            break;
+                hangmanImage.setAttribute("src", "assets/images/patti.jpg");
+                break;
             case "patrick":
-            hangmanImage.setAttribute("src", "assets/images/patrick.jpg");
-            break;
+                hangmanImage.setAttribute("src", "assets/images/patrick.jpg");
+                break;
             case "philbert":
-            hangmanImage.setAttribute("src", "assets/images/philbert.jpg");
-            break;
+                hangmanImage.setAttribute("src", "assets/images/philbert.jpg");
+                break;
             case "plank":
-            hangmanImage.setAttribute("src", "assets/images/plank.jpg");
-            break;
+                hangmanImage.setAttribute("src", "assets/images/plank.jpg");
+                break;
             case "prometheus":
-            hangmanImage.setAttribute("src", "assets/images/pro-bob.jpg");
-            break;
+                hangmanImage.setAttribute("src", "assets/images/pro-bob.jpg");
+                break;
             case "reggie":
-            hangmanImage.setAttribute("src", "assets/images/reggie.jpg");
-            break;
+                hangmanImage.setAttribute("src", "assets/images/reggie.jpg");
+                break;
             case "rocket power":
-            hangmanImage.setAttribute("src", "assets/images/rocket-power.jpg");
-            break;
+                hangmanImage.setAttribute("src", "assets/images/rocket-power.jpg");
+                break;
             case "rocko":
-            hangmanImage.setAttribute("src", "assets/images/rockos.jpg");
-            break;
+                hangmanImage.setAttribute("src", "assets/images/rockos.jpg");
+                break;
             case "roger klotz":
-            hangmanImage.setAttribute("src", "assets/images/roger.jpg");
-            break;
+                hangmanImage.setAttribute("src", "assets/images/roger.jpg");
+                break;
             case "rugrats":
-            hangmanImage.setAttribute("src", "assets/images/Rugrats.png");
-            break;
+                hangmanImage.setAttribute("src", "assets/images/Rugrats.png");
+                break;
             case "skeeter valentine":
-            hangmanImage.setAttribute("src", "assets/images/skeeter.jpg");
-            break;
+                hangmanImage.setAttribute("src", "assets/images/skeeter.jpg");
+                break;
             case "spongebob squarepants":
-            hangmanImage.setAttribute("src", "assets/images/spongebob.jpg");
-            break;
+                hangmanImage.setAttribute("src", "assets/images/spongebob.jpg");
+                break;
             case "squidward":
-            hangmanImage.setAttribute("src", "assets/images/squidward.jpg");
-            break;
+                hangmanImage.setAttribute("src", "assets/images/squidward.jpg");
+                break;
             case "tommy pickles":
-            hangmanImage.setAttribute("src", "assets/images/tommy.jpg");
-            break;            
+                hangmanImage.setAttribute("src", "assets/images/tommy.jpg");
+                break;
         }
-
         // Update the win counter in HTML
+        document.getElementById("button-grid").style.display = "grid";
         document.getElementById("winCounter").innerHTML = winCount;
-        setTimeout(startGame, 2500);
+        pause = true;
     }
 
-    // Check if usr lost
-    else if (guessesLeft == 0) {
+    // Check if user lost
+    else if (guessesLeft == 0 && pause === false) {
         lossCount++;
-
         // Update the loss counter in HTML
         document.getElementById("lossCounter").innerHTML = lossCount;
-        setTimeout(startGame, 1500);
+        document.getElementById("button-grid").style.display = "grid";
+        pause = true;
+
     }
 }
 
@@ -253,14 +241,13 @@ var changeHangman = () => {
 }
 
 // Hides the instructions text
-function hideIntro() {
+var hideIntro = () => {
     document.getElementById("intro-container").style.display = "none";
     document.getElementById("game-container").style.display = "grid";
 }
 
-// MAIN LOGIC
+// Main Logic
 //============================================================
-startGame();
 document.onkeydown = function (event) {
     hideIntro(event.key);
 
@@ -274,3 +261,5 @@ document.onkeydown = function (event) {
         document.getElementById("alert-text").textContent = ("Please select a valid letter.");
     }
 }
+
+reset();
